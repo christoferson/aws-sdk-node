58eb1ad97fe1d6f124af1cbeb7118c9f40350cd8
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-11-23 00:55:15 +0900

    Add Query by Partition Key

--- dynamo-client-read.js
+++ dynamo-client-read.js
@@ -2,21 +2,41 @@ const AWS = require("aws-sdk");
 
 console.log(AWS.VERSION);
 
-AWS.config.update({ region: '<region>' });
+AWS.config.update({ region: '<region>>' });
 
 const dynCLient = new AWS.DynamoDB.DocumentClient();
 
-// Query by Partition and Sort Key
-dynCLient.get({
-    TableName: '<table>', 
-    Key: {
-        Region: '<key>',
-        CharacterName: '<sort-key>'
+// Get by Partition and Sort Key
+// dynCLient.get({
+//     TableName: '<table>', 
+//     Key: {
+//         Region: 'US',
+//         CharacterName: 'Adria'
+//     }
+// }, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(JSON.stringify(data, null, 2));
+//     }
+// });
+
+//Query by Partition Key
+dynCLient.query({
+    TableName: '<table>',
+    KeyConditionExpression: "#region = :region",
+    ExpressionAttributeNames: {
+        "#region": "Region"
+    },
+    ExpressionAttributeValues: {
+        ':region': 'US'
     }
-}, (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(JSON.stringify(data, null, 2));
-    }
-});
+    }, 
+    (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(JSON.stringify(data, null, 2));
+        }
+    }   
+);
